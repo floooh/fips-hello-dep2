fab6d3ba39ebf9d638a53bd0d6170f9826fbff5c
Andre Weissflog <floooh@gmail.com>
2014-12-16 20:24:05 +0100

    Added a dep2 test lib and app

---
+++ src/dep2/dep2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_dep2() {
+    printf("Hello from dep2\n");
+}
+
---
+++ src/test/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "dep2/dep2.h"
+
+int main() {
+    print_dep2();
+    return 0;
+}
+
+
